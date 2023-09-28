40102f1bd744712a7d061a8bd67380396070ba4e
cxtao <1246784448@qq.com>
2023-09-29 03:18:07 +0800

    feat: 环境变量修改

--- apps/meeting/src/common/readEnv.ts
+++ apps/meeting/src/common/readEnv.ts
@@ -1,13 +1,15 @@
 import * as path from 'path';
 import * as fs from 'fs';
+import * as os from 'os';
 
+// 这里需要注意window 和 unix 系统的换行符是不一样的
 export const readEnv = () => {
   const urlPath = path.join(process.cwd(), './.env');
   const content = fs.readFileSync(urlPath, {
     encoding: 'utf-8',
   });
   content
-    .split('\r\n')
+    .split(os.EOL)
     .filter((item) => {
       return item !== '';
     })
